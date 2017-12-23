ded6e6708a4f4b609ae0bdd85fa6d6ae5b66e804
Christopher Chen <chrichen@paypal.com>
2017-12-23 14:13:11 -0800

    added a few tests and a before test

--- test/TestPuppies.js
+++ test/TestPuppies.js
@@ -1,29 +1,45 @@
 var Puppies = artifacts.require('Puppies');
 
-contract('Puppies', function(accounts){
+contract('Puppies', function (accounts) {
+    let instance;
+    let name = 'puppy1';
 
+    beforeEach('new contract per test', async function () {
+        instance = await Puppies.new(accounts[0]);
+    });
 
-
-    it("should return no puppies", async function(){
+    it('should return no puppies', async function () {
         let instance = await Puppies.deployed();
         let count = await instance.getCountOfPuppies();
         assert.equal(count, 0);
     });
 
-    it("should create a puppy", async function(){
+    it('should create a puppy', async function () {
         let instance = await Puppies.deployed();
-        var name = 'puppy1';
         await instance.createRandomPuppy(name);
         let count = await instance.getCountOfPuppies();
         assert.equal(count, 1);
+    });
 
-
-        // todo: not sure how to check the name of a puppy in js
-        var response = await instance.getPuppy(0);
+    // todo: not sure how to check the name of a puppy in js
+    it('should have name puppy1', async function(){
+        let instance = await Puppies.deployed();
+        let response = await instance.getPuppy(0);
         console.log('response:', response);
         // assert.equal(response.name.toString(), 'puppy1');
     });
 
+    // you can grab existing instance with deployed
+    it('should check existing contract', async function () {
+        let instance = await Puppies.deployed();
+        let count = await instance.getCountOfPuppies();
+        assert.equal(count, 1);
+    })
+
+    // otherwise instance will be a newly generated contract by the before
+    it('should check new contract', async function () {
+        assert.equal(await instance.getCountOfPuppies(), 0);
+    })
 
 
 });
