28841f45e968ec5f4736b5a4134d469a28a3a83b
Christopher Chen <chrichen@paypal.com>
2017-12-23 14:04:38 -0800

    test update

--- test/TestPuppies.js
+++ test/TestPuppies.js
@@ -2,12 +2,28 @@ var Puppies = artifacts.require('Puppies');
 
 contract('Puppies', function(accounts){
 
-    it("should return puppies", async function(){
 
+
+    it("should return no puppies", async function(){
         let instance = await Puppies.deployed();
-
-        let count = await instance.getCountOfPuppies.call();
-
+        let count = await instance.getCountOfPuppies();
+        assert.equal(count, 0);
     });
+
+    it("should create a puppy", async function(){
+        let instance = await Puppies.deployed();
+        var name = 'puppy1';
+        await instance.createRandomPuppy(name);
+        let count = await instance.getCountOfPuppies();
+        assert.equal(count, 1);
+
+
+        // todo: not sure how to check the name of a puppy in js
+        var response = await instance.getPuppy(0);
+        console.log('response:', response);
+        // assert.equal(response.name.toString(), 'puppy1');
+    });
+
+
 
 });
